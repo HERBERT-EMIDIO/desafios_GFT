481346c793830042d19e71bfb3287f371a859649
Herbert Emidio <herbertemidio2017@gmail.com>
2022-05-05 19:58:40 -0300

    Desafio de código gft

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+namespace teste
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Desafio de códigos GFT");
+        }
+    }
+}
